8f49e81d35dc21e8a38a159e0aa892d7601cd019
ezreum <whismater_power@hotmail.com>
2019-10-10 07:36:53 +0200

    ultima

--- clase/t3/uno/listado.php
+++ clase/t3/uno/listado.php
@@ -1,5 +1,9 @@
 <?php
 $coleccion=$_COOKIE['coleccionable'];
-
-
+echo "<table border=\"1\">";
+echo "<tr><th>NombreGalleta</th><th>ContenidoGalleta</th></tr>";
+foreach ($coleccion as $v => $k) {
+    echo "<tr><td>$v</td><td>$k</td></tr>";
+}
+echo "</table>";
 ?>
