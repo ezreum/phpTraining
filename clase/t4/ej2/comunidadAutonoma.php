ee2cab6fc21c572e0516c545244f2f7a82dc881c
ezreum <whismater_power@hotmail.com>
2019-10-28 07:51:18 +0100

    ej2Ajax casi

--- clase/t4/ej2/comunidadAutonoma.php
+++ clase/t4/ej2/comunidadAutonoma.php
@@ -1,11 +1,9 @@
-<?php require_once 'info.php';?>
-
+<?php require_once 'info.php';
+$caSelec = isset($_GET['comunidad'])?$_GET['comunidad']:'Andalucia';?>
 <html>
 <head>
 <title>Comunidades y provincias</title>
-<script type="text/javascript"
-	src="https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/form-serialize/serialize-0.2.min.js"></script>
-</script>
+
 <script>
 
 var x;
@@ -23,29 +21,43 @@ document.getElementById("provincia").innerHTML=res;
 
 function refrescar() {
 	x = new XMLHttpRequest();
-	var queryString = serialize(document.getElementById("provincia"));
-	x.open("GET", "ajaxCall.php?"+queryString, true);
+	var queryString = document.getElementById("provincia");
+	x.open("GET", "ajaxCall.php?provincia="+queryString, true);
 	x.send();
 	x.onreadystatechange = function(){
 		if (x.readyState==4 &&x.status==200){
             }
-            pintarSelect();
+            //pintarSelect();
 			}
-		}$inyeccion
+		}
 
 </script>
+
 </head>
 <body>
-<label for="comunidad">CCAA</label>
-<select id="comunidad" name="comunidad" onchange="refrescar();">
-<?php foreach($comunidades as $comunidad => $provincia) :?>
-	<option value="<?=$comunidad?>" ><?=$comunidad?></option>
-<?php endforeach; ?>
+
+	<form id="form">
+		<label for="comunidad">CCAA</label> <select id="comunidad"
+			name="comunidad" onchange="refrescar();">
+	<?php foreach($comunidades as $comunidad => $contenido) :?>
+	<option value="<?=$comunidad?>"
+				<?= $comunidad==$caSelec?'selected="selected"':'';?>>
+	<?= $comunidad ?>
+	</option>
+	<?php endforeach; ?>
 </select>
-<br/>
-<label for="provincia">Provincia</label>
-<select id="provincia" name="provincia">
-<option id = "nombres">{$comunidades['Andalucía'][0]}</option>
+	</form>
+
+	<label for="provincia">Provincia(s)</label>
+	<select id="provincia">
+
+	<?php foreach($comunidades[$caSelec] as $provincia): ?>
+<option>
+	<?= $provincia ?>
+</option>
+	<?php endforeach; ?>
+
 </select>
+
 </body>
 </html>
