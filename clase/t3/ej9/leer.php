df574ea16d53dcbc4e3040c72d2dcf0d1950bb2f
ezreum <whismater_power@hotmail.com>
2019-10-28 06:55:42 +0100

    9 actualizacion

--- clase/t3/ej9/leer.php
+++ clase/t3/ej9/leer.php
@@ -1,18 +1,15 @@
 <?php
 session_start();
 $usuario = isset($_GET['name'])?$_GET['name']:'';
-    echo "<table><tr><th>numero de mensaje</th><th></th><th>Remitente</th><th></th><th>Fecha</th><th></th><th></th><th>Texto</th></tr>";
     $numMen=1;
 foreach ($_SESSION['usuarios'][$_SESSION['_activo']]['mensajes'][$usuario] as $mensaje){
     foreach ($mensaje as $r => $mensajito){
-        echo "<tr>";
-        echo "<td>$numMen.</td><td></td> <td>{$mensajito['remitente']} </td><td></td><td>{$mensajito['fecha']}</td><td></td><td></td><td>{$mensajito['texto']}</td>";
-        echo "</tr>";
+        echo "<p>Numero de mensaje: $numMen. &emsp; Remitente: {$mensajito['remitente']}. &emsp; Fecha: {$mensajito['fecha']}</p>
+        <p>Mensaje: {$mensajito['texto']}</p>";
         $numMen++;
         }
+        echo "<hr/>";
     }
-    echo "</table>";
-
 echo <<<listaUsuario
     <a href="listaUsuarios.php">Bandeja de entrada</a>
 listaUsuario;
