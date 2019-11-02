bddf52fd4b30d49e538da858bcb026e07d0cf2ad
ezreum <whismaster_power@hotmail.com>
2019-11-02 18:39:58 +0100

    a

--- clase/t5/01/ej01Leer.php
+++ clase/t5/01/ej01Leer.php
@@ -1,8 +1,16 @@
 <?php
 $name = isset($_GET['nombre'])?$_GET['nombre']:'';
+require_once '../util.php';
+$base = conectarMySQL();
+$query = "insert into trabajadores(nombre) values('$name')";
 
-$query = "insert into ";
-
+if($base->exec($query)!=0){
+    echo "<h1>Fila insertada</h1>";
+}
+else {
+    echo "<h1>Error inexperado!</h1>";
+    die();
+}
 
 ?>
 <a href="ej01.php">volver atrás</a>
