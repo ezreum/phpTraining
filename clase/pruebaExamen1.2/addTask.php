ed3b687c15d454d54890add74a26fe9e211015e1
ezreum <whismaster_power@hotmail.com>
2019-11-06 18:35:46 +0100

    practica 1.2 hecha

--- clase/pruebaExamen1.2/addTask.php
+++ clase/pruebaExamen1.2/addTask.php
@@ -1,2 +1,28 @@
-<?php
-?>
+<?php session_start() ?>
+<html>
+
+	<head>
+	
+	<title>Add Task</title>
+	
+	</head>
+
+	<body>
+	
+	<form action="addPost.php" method="POST">
+	<label>nombre de la tarea</label>
+	<input type="text" name="tarea"/>
+	<br/>
+	<label>empleado</label>
+	<select name="empleado">
+	<option>Pepe</option>
+	<option>Ana</option>
+	<option>Juan</option>
+	</select>
+	<br/>
+	<input type="submit" value="asignar"/>
+	</form>
+	<a href="panel.php">volver al panel</a>
+	</body>
+
+</html>
--- clase/pruebaExamen1.2/panel.php
+++ clase/pruebaExamen1.2/panel.php
@@ -1,4 +1,22 @@
-<?php
+<?php session_start() ?>
+<html>
+	<head>
+	<meta charset="utf-8" />
+	
+	<title>Gestión de tareas</title>
+	
+	</head>
 
-
-?>
+	<body>
+	<h1>Gestión de tareas</h1>
+	<form action="addTask.php">
+		<input type="submit" value="añadir tarea"/>
+	</form>
+	
+	<form action="seeTask.php">
+		<input type="submit" value="ver tarea"/>
+	</form>
+		
+	</body>
+	
+</html>
