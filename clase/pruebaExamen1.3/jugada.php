7b8fdbfb1477aa5417e6e21039ea6a62cf9b287f
ezreum <whismaster_power@hotmail.com>
2019-11-07 23:18:03 +0100

    jugada comprobada

--- clase/pruebaExamen1.3/jugada.php
+++ clase/pruebaExamen1.3/jugada.php
@@ -1,39 +1,53 @@
 <?php
-session_start();
 require_once 'tools.php';
+require_once 'Carta.php';
+session_start();
 isset($_SESSION['baraja'])?'':header('Location:inicializar.php');
-$bool = false;
+$quitar='';
+$numero=1;
 ?>
 
 
 <h1>Siete y media</h1>
 
-<a href="jugadaPost.php">
-	<button>sacar carta</button>
-</a>
-
 <h3>Jugada</h3>
 <?php 
 if (isset($_SESSION['jugador'])&& $_SESSION['jugador']!==[]):
-/* $imagenCarta = <<<img
- <img src="" alt="carta"/>
- img;*/
+
+
 
 ?>
 
 <?php $acc=0;?>
-	<ul>
+	<table>
+	<tr>
 	<?php foreach ($_SESSION['jugador'] as $carta): ?>
-		<li>
-			<?php $acc += $carta->valor ?>
-			<?= $carta->nombre?> (total: <?= $acc ?>)
-		</li>
+		<td>
+			<?php 
+			$imagenCarta = <<<img
+ <img src="$carta->img" alt="carta" width="150px" height="200px"/>
+img;
+			$acc += $carta->valor ?>
+			<?= $imagenCarta?> (total: <?= $acc ?>)
+			<?php if ($acc>7.5){
+			    $numero=$acc;
+			
+			}?>
+		</td>
 	<?php endforeach;?>
-</ul>
+	
+</tr>
+</table>
+
+<?= $numero>7.5?'<h3>Te has pasado, has perdido</h3>':''; ?>
 
 <?php else:?>
 <p>(Todavia no se han jugado cartas)</p>
 <?php endif;?>
+
+<a href="jugadaPost.php">
+	<button <?= $numero>7.5?'disabled':'';?>>sacar carta</button>
+</a>
 
 <a href="jugadaBanca.php">
 	<button>plantarse</button>
--- clase/pruebaExamen1.3/tools.php
+++ clase/pruebaExamen1.3/tools.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require_once 'Carta.php';
 
 function prepararBaraja() {
