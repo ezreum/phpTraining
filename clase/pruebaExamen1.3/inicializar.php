8a823ab7a9e706ef6548df81e223f94c18cc4b50
josito <josito@josito-VirtualBox>
2019-11-11 11:01:41 +0100

    correccion

--- clase/pruebaExamen1.3/inicializar.php
+++ clase/pruebaExamen1.3/inicializar.php
@@ -2,16 +2,16 @@
 session_start();
 require_once 'tools.php';
 
-if (isset($_GET['nuevo'])){
-    unset($_SESSION['baraja']);
-}
+// if (isset($_GET['nuevo'])){
+//     unset($_SESSION['baraja']);
+// }
 
 
 
-if (!isset($_SESSION['baraja'])){
+/* if (!isset($_SESSION['baraja'])){ */
     $_SESSION['jugador']=[];
     $_SESSION['baraja'] = prepararBaraja();
-}
+
 if ( !isset($_SESSION['ganadas']) && !isset($_SESSION['perdidas'])) {
     $_SESSION['ganadas']=0;
     $_SESSION['perdidas']=0;
