f8a5ee65bc2123267cd5effed96d0063a41e8246
ezreum <whismaster_power@hotmail.com>
2019-11-13 20:55:11 +0100

    actualizacion upd id

--- clase/t5/ej04/updUsu.php
+++ clase/t5/ej04/updUsu.php
@@ -1,24 +1,10 @@
 <body>
 <form action="updUsuPost.php" method="POST">
 <fieldset>
-<legend>Seleccionar trabajador a modificar por alguno de sus rasgos</legend>
+<legend>Seleccionar trabajador a modificar por alguno de su id</legend>
 
 <h3>id</h3>
 <input name="id" type="text"/>
-
-<h3>nombre</h3>
-<input name="nom" type="text"/>
-
-<h3>apellido</h3>
-<input name="ape" type="text"/>
-
-<h3>teléfono</h3>
-<input name="telf" type="text"/>
-
-<h3>sexo</h3>
-<input name="sexo" type="radio" value="H" />H
-<input name="sexo" type="radio" value="M" />M
-<input name="sexo" type="radio" value="U" />U
 
 <input type="submit" value="buscar empleado"/>
 </fieldset>
