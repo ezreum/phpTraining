83db2f13ec2eb0d6eab24d5a6642bbce656f604f
josito <josito@josito-VirtualBox>
2019-11-13 14:19:37 +0100

    rest

---
+++ clase/t5/ej04/panel.php
@@ -0,0 +1,14 @@
+<h1>Bienvenido a la aplicación</h1>
+<h3>Selecciona la función que quieres ejecutar</h3>
+
+<h3>Acciones posibles</h3>
+<a href="crearUsu.php"><button>crear usuario</button></a>
+
+
+<a href="leerUsu.php"><button>leer usuarios</button></a>
+
+
+<a href="updUsu.php"><button>modificar usuario</button></a>
+
+
+<a href="delUsu.php"><button>eliminar usuario</button></a>
