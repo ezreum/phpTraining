da507ff6c74cea31996d8f26c94c864506593d42
ezreum <whismaster_power@hotmail.com>
2020-05-21 13:15:02 +0200

    cambios en csv

--- clase/t2/leerEscribirCSV.php
+++ clase/t2/leerEscribirCSV.php
@@ -8,12 +8,11 @@ $sitio = fopen('resultado.csv', 'a+');
 fputcsv($sitio, ['provincia','minimo','maximo'], ',');
 fclose($sitio);
 fgetcsv($file);
-while(! feof($file))
+while($info = fgetcsv($file))
 {
-    $info = fgetcsv($file);
     $infoAux = explode(',', $info[0]);
     $infoLinea = explode(';', $infoAux[0]);
-   
+    
     if ($max==0 && $min==0) {
         $actual = $infoLinea[0];
         $max=$infoLinea[2];
@@ -23,15 +22,16 @@ while(! feof($file))
         $paquete[0] = $actual;
         $paquete[1] = $min;
         $paquete[2] = $max;
-        $sitio = fopen('resultado.csv', 'a+');
-        fputcsv($sitio, $paquete, ',');
-        fclose($sitio);
+        $destino = fopen('resultado.csv', 'a+');
+        fputcsv($destino, $paquete, ',');
+        fclose($destino);
         $actual = $infoLinea[0];
-        $max=0;
-        $min=100000;
+        $max = 0;
+        $min =0;
     }
-    $max<$infoLinea[2]?$max=$infoLinea[2]:'';
-    $min>$infoLinea[2]?$min=$infoLinea[2]:'';
+    
+    isset($infoLinea[2]) && !empty($infoLinea[2]) && $max<$infoLinea[2]?$max=$infoLinea[2]:'';
+    (isset($infoLinea[2]) && !empty($infoLinea[2]) && ($min>$infoLinea[2]) || $min==0)?$min=$infoLinea[2]:'';
 }
 
 fclose($file);
